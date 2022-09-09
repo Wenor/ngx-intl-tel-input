fd1f313ca2c98d132dd953eeb65fa5d6f1d5787f
Oleksandr <a.dudko@trip-top.com>
2022-09-09 17:45:21 +0300

    lib version

--- projects/ngx-intl-tel-input/src/lib/services/ngx-intl-tel-input-error-matcher.ts
+++ projects/ngx-intl-tel-input/src/lib/services/ngx-intl-tel-input-error-matcher.ts
@@ -1,11 +1,11 @@
+import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
-import {UntypedFormControl, FormGroupDirective, NgForm} from '@angular/forms';
 
 export class NgxIntlTelInputErrorMatcher implements ErrorStateMatcher {
-  constructor(private customControl: UntypedFormControl) {
+  constructor(private customControl: FormControl) {
   }
 
-  isErrorState(control: UntypedFormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(this.customControl && this.customControl.invalid
       && ((this.customControl.dirty && this.customControl.touched) || isSubmitted));
